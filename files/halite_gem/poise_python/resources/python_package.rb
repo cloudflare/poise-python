203e9b330bbd31b307f98a72f4ec374196064444
Harry Cao <harry@area1security.com>
2019-06-24 22:17:13 -0700

    update metadata.rb

--- files/halite_gem/poise_python/resources/python_package.rb
+++ files/halite_gem/poise_python/resources/python_package.rb
@@ -46,17 +46,11 @@ try:
   from pip.commands import InstallCommand
   from pip.index import PackageFinder
   from pip.req import InstallRequirement
-  install_req_from_line = InstallRequirement.from_line
 except ImportError:
   # Pip 10 moved all internals to their own package.
   from pip._internal.commands import InstallCommand
   from pip._internal.index import PackageFinder
-  try:
-    # Pip 18.1 moved from_line to the constructors
-    from pip._internal.req.constructors import install_req_from_line
-  except ImportError:
-    from pip._internal.req import InstallRequirement
-    install_req_from_line = InstallRequirement.from_line
+  from pip._internal.req import InstallRequirement
 
 packages = {}
 cmd = InstallCommand()
@@ -79,7 +73,7 @@ with cmd._build_session(options) as session:
   finder = PackageFinder(**finder_options)
   find_all = getattr(finder, 'find_all_candidates', getattr(finder, '_find_all_versions', None))
   for arg in args:
-    req = install_req_from_line(arg)
+    req = InstallRequirement.from_line(arg)
     found = finder.find_requirement(req, True)
     all_candidates = find_all(req.name)
     candidate = [c for c in all_candidates if c.location == found]
